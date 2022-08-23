9f52523e3139347ae8941f824b8ca2bc442dfa2e
Matt Lee <matt.jay.lee@gmail.com>
2022-08-23 11:10:15 -0700

    Code initialization to set up game and choose word

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,30 @@
 class Game
 
+    attr_accessor :solve_word, :letters_guessed, :guesses_remaining, :current_puzzle
+
+    def initialize
+        @solve_word = choose_word()
+        @letters_guessed = []
+        @guesses_remaining = 6
+        @current_puzzle = Array.new(@solve_word.length, '_')
+    end
+
     def load_dictionary
         dictionary = File.read("dictionary.txt").split
         solve_word = dictionary[rand(dictionary.length)]
-        puts solve_word
+    end
+
+    def choose_word
+        solve_word = load_dictionary()
+        if solve_word.length > 4 && solve_word.length < 13
+            solve_word
+        else
+            choose_word()
+        end
+    end
+
+    def display
+
     end
 
 end
