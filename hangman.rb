078169ef7c2d238115e0e2c903711bd6dc359814
Matt Lee <matt.jay.lee@gmail.com>
2022-09-05 17:54:52 -0700

    Code load/save functions and clean functions

--- hangman.rb
+++ hangman.rb
@@ -2,18 +2,14 @@ require 'yaml'
 
 class Game
 
-    attr_accessor :solve_word, :letters_guessed, :guesses_remaining, :current_puzzle, :game_id
-
-    @@game_counter = 0
-
     def initialize
-        @@game_counter += 1
         @solve_word = choose_word()
         @letters_guessed = []
         @guesses_remaining = 6
         @current_puzzle = Array.new(@solve_word.length, '_')
-        @game_id = @@game_counter
         @player = Player.new
+        @game_id = get_counter()
+        puts "Current puzzle is #{@current_puzzle.join}"
     end
 
     def load_dictionary
@@ -33,7 +29,7 @@ class Game
     def display
         puts "The current puzzle is #{@current_puzzle.join}." 
         puts "You have guessed #{@letters_guessed}."
-        puts "You have #{@guesses_remaining} guesses remaining #{solve_word} game id #{@game_id}"
+        puts "You have #{@guesses_remaining} guesses remaining"
     end
 
     def play_turn
@@ -47,10 +43,17 @@ class Game
         case letter
         when 'save' 
             save()
-            puts "GAME PROGRESS SAVED SUCCESSFULLY AS GAME #{game_id}"
             play_turn()
-        when 'load' then load()
-        when 'a'..'z' then letter
+        when 'load'
+            load()
+            display()
+            play_turn()
+        when 'new' then new_game?()
+        when 'a'..'z' 
+            if letter.length === 1
+                letter
+            else play_turn()
+            end
         else play_turn()
         end
     end
@@ -74,6 +77,7 @@ class Game
             new_game?()
         elsif guesses_remaining === 0
             puts "YOU DID NOT SOLVE THE PUZZLE IN THE ALOTTED GUESSES"
+            puts "THE WORD WAS #{@solve_word}"
             new_game?()
         end
     end
@@ -86,22 +90,31 @@ class Game
             :current_puzzle => @current_puzzle,
             :game_id => @game_id
         })
-        if Dir.exists?("saved_games")
-            File.write("./saved_games/#{game_id}", data)
-        else
+        unless Dir.exists?("saved_games")
             Dir.mkdir("saved_games")
-            File.write("./saved_games/#{game_id}", data)
         end
+        unless File.exist?("saved_games/#{@game_id}")
+            increment_counter()
+        end
+        File.write("./saved_games/#{@game_id}", data)
+        puts "GAME PROGRESS SAVED SUCCESSFULLY AS GAME #{@game_id}"
     end
 
-    def load(save_file)
-        data = YAML.load save_file
-        @solve_word = data[:solve_word]
-        @letters_guessed = data[:letters_guessed]
-        @guesses_remaining = data[:guesses_remaining]
-        @current_puzzle = data[:current_puzzle]
-        @game_id = data[:game_id]
-        p @solve_word, @letters_guessed, @guesses_remaining, @current_puzzle, @game_id
+    def load()
+        files = Dir.children("saved_games")
+        puts "Please enter a game id to load a saved game"
+        puts files
+        game_to_load = gets.chomp
+        if game_to_load.to_s in files
+            data = YAML.load(File.read("./saved_games/#{game_to_load}"))
+            @solve_word = data[:solve_word]
+            @letters_guessed = data[:letters_guessed]
+            @guesses_remaining = data[:guesses_remaining]
+            @current_puzzle = data[:current_puzzle]
+            @game_id = data[:game_id]
+            puts "SUCCESSFULLY LOADED GAME #{@game_id}"
+        else play_turn()
+        end
     end
 
     def new_game?
@@ -117,23 +130,36 @@ class Game
         end
     end
 
+    def get_counter
+        if Dir.exists?("game_counter")
+            file = File.open("./game_counter/counter")
+            file.read
+        else Dir.mkdir("game_counter")
+            File.write("./game_counter/counter", 1)
+            1
+        end
+    end
+
+    def increment_counter
+        file = File.open("./game_counter/counter", "r+")
+        counter = file.read
+        counter = counter.to_i + 1
+        file.seek(0)
+        file.write(counter)
+        file.close
+        counter
+    end
+
 end
 
 class Player
 
     def guess_letter
-        puts "Please input a letter to guess. You may also input 'save' or 'load'"
+        puts "Input a letter to guess. You may also input 'save', 'load', or 'new'."
         letter = gets.chomp.downcase
     end
 
 end
-
-# game = Game.new
-# game2 = Game.new
-# player = Player.new
-# game2.check_guess(game2.solve_word, player.guess_letter)
-# save = game2.save
-# game2.load(save)
 
 game = Game.new
 game.play_turn
